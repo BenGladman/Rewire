258a9e4550a0265950967ca65fa64cc4c8be8b75
Ben Gladman <Ben Gladman>
2016-07-04 21:25:34 +0100

    Start demo

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,42 +2,32 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BoxDefinition, WireDefinition } from "../types";
 import Rewire from "../components/Rewire";
-import { squareJackType, arrowJackType } from "../components/JackType";
+import { circleJackType, squareJackType, arrowJackType } from "../components/JackType";
+import "./index.css";
 
 const initialBoxes: BoxDefinition[] = [
     {
-        x: 50, y: 50, width: 100, height: 100,
-        content: <div>Box A</div>,
+        x: 200, y: 100, width: 200, height: 50,
+        content: <h1 className="demo-h1">Rewire!</h1>,
         sockets: [
-            { side: "right", pos: 0.2, type: arrowJackType },
-            { side: "top", pos: 0, type: arrowJackType },
-            { side: "bottom", pos: 0, type: arrowJackType },
-            { side: "left", pos: 0.2, type: arrowJackType },
+            { side: "left", pos: -0.2, type: squareJackType },
+            { side: "right", pos: 0.2, type: squareJackType },
+            { side: "bottom", pos: 0, type: circleJackType },
         ]
     },
     {
-        x: 250, y: 250, width: 100, height: 100,
-        content: <div>Box B</div>,
+        x: 50, y: 50, width: 100, height: 100,
+        content: <p className="demo-p">A wiring UI written in React and Typescript</p>,
         sockets: [
-            { side: "top", pos: 0, type: arrowJackType },
-            { side: "bottom", pos: -0.2, type: arrowJackType },
-            { side: "bottom", pos: 0.2, type: squareJackType },
+            { side: "right", pos: 0, type: arrowJackType },
         ]
     }
 ];
 
 const initialWires: WireDefinition[] = [
     {
-        jack1: { socket: initialBoxes[0].sockets[0], type: arrowJackType },
+        jack1: { socket: initialBoxes[0].sockets[0], type: squareJackType },
         jack2: { socket: initialBoxes[1].sockets[0], type: arrowJackType },
-    },
-    {
-        jack1: { socket: initialBoxes[0].sockets[1], type: arrowJackType },
-        jack2: { socket: initialBoxes[1].sockets[1], type: arrowJackType },
-    },
-    {
-        jack1: { socket: initialBoxes[0].sockets[2], type: arrowJackType },
-        jack2: { socket: initialBoxes[0].sockets[3], type: arrowJackType }
     }
 ];
 
