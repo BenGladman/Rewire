a9cc0f9165e0ba90de20fdf2e80a2c4f8f9326b5
Ben Gladman <Ben Gladman>
2016-07-04 22:03:08 +0100

    Demo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rewire
 Wiring UI in React + Typescript
+
+Demo at https://bengladman.github.io/Rewire/
--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -16,10 +16,24 @@ const initialBoxes: BoxDefinition[] = [
         ]
     },
     {
-        x: 50, y: 50, width: 100, height: 100,
+        x: 50, y: 60, width: 100, height: 100,
         content: <p className="demo-p">A wiring UI written in React and Typescript</p>,
         sockets: [
             { side: "right", pos: 0, type: arrowJackType },
+        ]
+    },
+    {
+        x: 400, y: 250, width: 80, height: 80,
+        content: <p className="demo-p">By<br />Ben<br />Gladman</p>,
+        sockets: [
+            { side: "top", pos: 0, type: circleJackType },
+        ]
+    },
+    {
+        x: 80, y: 300, width: 180, height: 40,
+        content: <p className="demo-p">View source on <a href="https://github.com/BenGladman/Rewire">GitHub</a></p>,
+        sockets: [
+            { side: "top", pos: -0.4, type: circleJackType },
         ]
     }
 ];
@@ -28,6 +42,14 @@ const initialWires: WireDefinition[] = [
     {
         jack1: { socket: initialBoxes[0].sockets[0], type: squareJackType },
         jack2: { socket: initialBoxes[1].sockets[0], type: arrowJackType },
+    },
+    {
+        jack1: { socket: initialBoxes[0].sockets[1], type: squareJackType },
+        jack2: { socket: initialBoxes[2].sockets[0], type: circleJackType },
+    },
+    {
+        jack1: { socket: initialBoxes[0].sockets[2], type: circleJackType },
+        jack2: { socket: initialBoxes[3].sockets[0], type: circleJackType },
     }
 ];
 
